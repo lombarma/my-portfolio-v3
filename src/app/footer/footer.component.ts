dd83e44f1f11edd54ff5752110fc5191cf207fac
Maxime LOMBARDO <maxime.lombardo@ext.bpifrance.fr>
2025-05-14 14:23:12 +0200

    fix(footer/header): fix routing

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { LinksBarComponent } from './links-bar/links-bar.component';
 
 @Component({
   selector: 'app-footer',
-  imports: [LinksBarComponent],
+  imports: [LinksBarComponent, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
