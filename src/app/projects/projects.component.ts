8e979f0b30ca4663cb1bb7bdcf87f92e5f2a41df
Maxime LOMBARDO <maxime.lombardo@ext.bpifrance.fr>
2025-05-14 21:31:51 +0200

    feat(projects): add header inside the page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { InsideHeaderComponent } from '../shared/inside-header/inside-header.component';
 import { PROJECTS } from './data/data.constant';
 import { ProjectCardComponent } from './project-card/project-card.component';
 import { Card } from './types/card.type';
 
 @Component({
   selector: 'app-projects',
-  imports: [ProjectCardComponent],
+  imports: [ProjectCardComponent, InsideHeaderComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
--- src/app/shared/inside-header/inside-header.component.ts
+++ src/app/shared/inside-header/inside-header.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-inside-header',
   imports: [],
   templateUrl: './inside-header.component.html',
-  styleUrl: './inside-header.component.scss'
+  styleUrl: './inside-header.component.scss',
 })
 export class InsideHeaderComponent {
-
+  readonly header = input<string>();
+  readonly title = input<string>();
+  readonly description = input<string>();
 }
