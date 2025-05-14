0ff339813b5e977a609ef7290a8696fc2cbee6be
Maxime Lombardo <maxime.lombardo2@edu.esiee.fr>
2025-05-15 00:04:04 +0200

    fix(images): set images as webp instead of png for loading

--- src/app/projects/data/data.constant.ts
+++ src/app/projects/data/data.constant.ts
@@ -7,7 +7,7 @@ export const PROJECTS: Card[] = [
       'Dynamic portfolio generator that creates personalized portfolios based on user input, showcasing skills, experiences and projects.',
     tags: ['Angular', 'NodeJs(express)', 'PostgreSQL', 'Docker', 'Kubernetes'],
     picture: {
-      path: 'assets/images/portfolio-generator.png',
+      path: 'assets/images/portfolio-generator.webp',
       altDescrption: 'Portfolio Generator Screenshot',
     },
     repoUrl: 'https://github.com/lombarma/portfolio-generator-v2',
@@ -18,7 +18,7 @@ export const PROJECTS: Card[] = [
       'Social networking platform for students, allowing association, event organization, and communication through posts and messages.',
     tags: ['Python(Django)', 'MySQL', 'HTML/CSS', 'Project Management'],
     picture: {
-      path: 'assets/images/social-network.png',
+      path: 'assets/images/social-network.webp',
       altDescrption: 'Social Network Screenshot',
     },
     repoUrl: 'https://github.com/MathisPipart/Nexus',
@@ -29,7 +29,7 @@ export const PROJECTS: Card[] = [
       'Machine learning model that predicts house prices based on various features such as location, size, and amenities.',
     tags: ['Python', 'Machine Learning', 'Data preprocessing', 'Keras'],
     picture: {
-      path: 'assets/images/house-prices.png',
+      path: 'assets/images/house-prices.webp',
       altDescrption: 'House Prices Prediction Screenshot',
     },
     repoUrl: 'https://github.com/lombarma/HousePricePredicition',
@@ -40,7 +40,7 @@ export const PROJECTS: Card[] = [
       'Weather forecasting application that provides real-time weather updates, forecasts, and alerts based on user location.',
     tags: ['Angular', 'API Integration', 'Responsive Design'],
     picture: {
-      path: 'assets/images/weather-app.png',
+      path: 'assets/images/weather-app.webp',
       altDescrption: 'Weather App Screenshot',
     },
     repoUrl: 'https://github.com/lombarma/MyMeteoApp',
@@ -51,7 +51,7 @@ export const PROJECTS: Card[] = [
       'Deep learning model that recognizes handwritten characters and digits, trained on a large dataset of handwritten samples.',
     tags: ['Python', 'TensorFlow', 'Image Processing', 'Deep Learning'],
     picture: {
-      path: 'assets/images/handwritten-digit-recognition.png',
+      path: 'assets/images/handwritten-digit-recognition.webp',
       altDescrption: 'Handwriting Recognition Screenshot',
     },
     repoUrl: 'https://github.com/lombarma/DLProject',
