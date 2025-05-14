228e84df1ee318c381b566337be0fef2c5deb702
Maxime LOMBARDO <maxime.lombardo@ext.bpifrance.fr>
2025-05-14 14:22:43 +0200

    feat(pages): add inside-header for all pages

---
+++ src/app/shared/inside-header/inside-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inside-header',
+  imports: [],
+  templateUrl: './inside-header.component.html',
+  styleUrl: './inside-header.component.scss'
+})
+export class InsideHeaderComponent {
+
+}
