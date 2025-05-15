0b922a54f7c7ac603838227e9032f4b0bd0eb405
Maxime Lombardo <maxime.lombardo2@edu.esiee.fr>
2025-05-15 23:43:54 +0200

    fix(all): update details

--- src/app/home/collaboration/collaboration.component.ts
+++ src/app/home/collaboration/collaboration.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
   selector: 'app-collaboration',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, RouterLink],
   templateUrl: './collaboration.component.html',
   styleUrl: './collaboration.component.scss',
 })
--- src/app/home/landing-section/landing-section.component.ts
+++ src/app/home/landing-section/landing-section.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ButtonComponent } from '../../shared/button/button.component';
 import { CodeMockupComponent } from './code-mockup/code-mockup.component';
 
 @Component({
   selector: 'app-landing-section',
-  imports: [CodeMockupComponent, ButtonComponent],
+  imports: [CodeMockupComponent, ButtonComponent, RouterLink],
   templateUrl: './landing-section.component.html',
   styleUrl: './landing-section.component.scss',
 })
